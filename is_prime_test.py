613ade50384c22953a96e090959abde0f89b04ff
Acher0ns <kamron.cole13@gmail.com>
2022-02-07 17:49:04 -0500

    Intermediary Step: Regression Testing

--- is_prime_test.py
+++ is_prime_test.py
@@ -54,3 +54,108 @@ def test_is_prime_1():
     actual = is_prime(number) # invoke function and get actaul result
     # analyse
     assert actual == expected
+
+
+# Tests added as part of intermediary step regression testing.
+def test_is_prime_non_prime_neg_10():
+    '''
+    Test is_prime with number prime number -10
+    '''
+    # setup
+    number = -10 # number to be passed as argument in invoke step
+    expected = False # expected result
+    # invoke
+    actual = is_prime(number) # invoke function and get actaul result
+    # analyse
+    assert actual == expected
+
+
+def test_is_prime_prime_neg_11():
+    '''
+    Test is_prime with number prime number -11
+    '''
+    # setup
+    number = -11 # number to be passed as argument in invoke step
+    expected = False # expected result
+    # invoke
+    actual = is_prime(number) # invoke function and get actaul result
+    # analyse
+    assert actual == expected
+
+
+def test_is_prime_2():
+    '''
+    Test is_prime with number 2
+    '''
+    # setup
+    number = 2 # number to be passed as argument in invoke step
+    expected = True # expected result
+    # invoke
+    actual = is_prime(number) # invoke function and get actaul result
+    # analyse
+    assert actual == expected
+
+
+def test_is_prime_3():
+    '''
+    Test is_prime with number 3
+    '''
+    # setup
+    number = 3 # number to be passed as argument in invoke step
+    expected = True # expected result
+    # invoke
+    actual = is_prime(number) # invoke function and get actaul result
+    # analyse
+    assert actual == expected
+
+
+def test_is_prime_10():
+    '''
+    Test is_prime with number 10
+    '''
+    # setup
+    number = 10 # number to be passed as argument in invoke step
+    expected = False # expected result
+    # invoke
+    actual = is_prime(number) # invoke function and get actaul result
+    # analyse
+    assert actual == expected
+
+
+def test_is_prime_23():
+    '''
+    Test is_prime with number 23
+    '''
+    # setup
+    number = 23 # number to be passed as argument in invoke step
+    expected = True # expected result
+    # invoke
+    actual = is_prime(number) # invoke function and get actaul result
+    # analyse
+    assert actual == expected
+
+
+def test_is_prime_24():
+    '''
+    Test is_prime with number 24
+    '''
+    # setup
+    number = 24 # number to be passed as argument in invoke step
+    expected = False # expected result
+    # invoke
+    actual = is_prime(number) # invoke function and get actaul result
+    # analyse
+    assert actual == expected
+
+
+def test_is_prime_100():
+    '''
+    Test is_prime with number 23
+    '''
+    # setup
+    number = 100 # number to be passed as argument in invoke step
+    expected = False # expected result
+    # invoke
+    actual = is_prime(number) # invoke function and get actaul result
+    # analyse
+    assert actual == expected
